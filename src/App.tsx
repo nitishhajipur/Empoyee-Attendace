f0582b47d2d4ffd9c727288cce632df748453f3d
Akshay mishra <akshaymishra1228@gmail.com>
2024-01-04 23:52:17 +0530

    Project setup and UI changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+// src/App.tsx
 
+import React from 'react';
+import Dashboard from './component/Dashboard/Dashboard';
+// import Navbar from './component/Dashboard/Navbar';
+import AppNavbar from './component/Dashboard/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NoPage from './component/Dashboard/NoPage';
+import Layout from './component/Dashboard/Navbar';
 import './App.css';
+import Calender from './component/Dashboard/Calender';
+import Employee from './component/Dashboard/Employee';
+import Project from './component/Dashboard/Project';
+
 
 function App() {
   return (
-    <div className="">
-      Empoyee Attendance
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='/calender' element={<Calender />} />
+          <Route path='/project' element={<Project />} />
+          <Route path='/employee' element={<Employee />} />
+
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
