77cf2eeb82c52d426e0fecee7177675f8bcf7620
Nitish <nitishhajipur01@gmail.com>
2024-01-06 15:36:36 +0530

    chanes for timesheet

--- src/component/Dashboard/Timesheet.tsx
+++ src/component/Dashboard/Timesheet.tsx
@@ -1,54 +1,31 @@
-import React from "react";
-import { useState } from 'react'
+import React, { useState } from "react";
+import './timesheet.css'
 const data = [
     {
-      employeeId: '01',
-      name: 'John Doe',
-      email: 'johndoe@email.com',
-      position: 'Frontend Developer',
+      date: '01/05/2024',
+      startTime: '9:00',
+      finishTime: '6:00',
+      regularHours: '8:00',
       overtime: '02',
-      sick: 'Sara',
-      vacation: 'sara@email.com',
-      holiday: 'HR Executive',
+      sick: '01',
+      vacation: '01',
+      holiday: '04',
       otherhrs: "10",
       totalhrs: "20",
     },
-    {
-      employeeId: '02',
-      name: 'Sara',
-      email: 'sara@email.com',
-      position: 'HR Executive',
-      overtime: '02',
-      sick: 'Sara',
-      vacation: 'sara@email.com',
-      holiday: 'HR Executive',
-      otherhrs: "10",
-      totalhrs: "20",
-    },
-    {
-      employeeId: '03',
-      name: 'Mike',
-      email: 'mike@email.com',
-      position: 'Backend Developer',
-      overtime: '02',
-      sick: 'Sara',
-      vacation: 'sara@email.com',
-      holiday: 'HR Executive',
-      otherhrs: "10",
-      totalhrs: "20",
-    },
+   
+   
   ]
 export default function Timesheet() {
     const [employeeData, setEmployeeData] = useState(data)
 
     const onChangeInput = (e:any, employeeId:any) => {
-      const { name, value } = e.target
+      const { name, value } = e.target 
+      // const editData = employeeData.map((item) =>
+      //   item.employeeId === employeeId && name ? { ...item, [name]: value } : item
+      // )
   
-      const editData = employeeData.map((item) =>
-        item.employeeId === employeeId && name ? { ...item, [name]: value } : item
-      )
-  
-      setEmployeeData(editData)
+      // setEmployeeData(editData)
     }
   return (
     <div className="timeSheet">
@@ -69,60 +46,97 @@ export default function Timesheet() {
         </tr>
       </thead>
       <tbody>
-        {employeeData.map(({ employeeId, name, email, position }) => (
-          <tr key={employeeId}>
+        {employeeData.map(({date,startTime,finishTime,regularHours,overtime,sick,vacation,holiday,otherhrs,totalhrs},index:any) => (
+          <tr key={index}>
+            <>{console.log("51...",date,startTime,finishTime,regularHours,overtime,sick,vacation,holiday,otherhrs,totalhrs)}</>
             <td>
               <input
                 name="date"
-                // value={name}
+                // value={data.date}
                 type="date"
-                onChange={(e) => onChangeInput(e, employeeId)}
-                placeholder="Type Name"
+                // onChange={(e) => onChangeInput(e, employeeId)}
+                // placeholder="Type Name"
               />
             </td>
             <td>
               <input
                 name="email"
-                value={email}
-                type="text"
-                onChange={(e) => onChangeInput(e, employeeId)}
-                placeholder="Type Email"
+                // value={data.finishTime}
+                type="number"
+                // onChange={(e) => onChangeInput(e, employeeId)}
+                // placeholder="Type Email"
               />
             </td>
             <td>
               <input
                 name="position"
-                type="text"
-                value={position}
-                onChange={(e) => onChangeInput(e, employeeId)}
-                placeholder="Type Position"
+                type="number"
+                // value={position}
+                // onChange={(e) => onChangeInput(e, employeeId)}
+                // placeholder="Type Position"
               />
             </td>
             <td>
               <input
                 name="position"
-                type="text"
-                value={position}
-                onChange={(e) => onChangeInput(e, employeeId)}
-                placeholder="Type Position"
+                type="number"
+                // value={position}
+                // onChange={(e) => onChangeInput(e, employeeId)}
+                // placeholder="Type Position"
               />
             </td>
             <td>
               <input
                 name="position"
-                type="text"
-                value={position}
-                onChange={(e) => onChangeInput(e, employeeId)}
-                placeholder="Type Position"
+                type="number"
+                // value={position}
+                // onChange={(e) => onChangeInput(e, employeeId)}
+                // placeholder="Type Position"
               />
             </td>
             <td>
               <input
                 name="position"
-                type="text"
-                value={position}
-                onChange={(e) => onChangeInput(e, employeeId)}
-                placeholder="Type Position"
+                type="number"
+                // value={position}
+                // onChange={(e) => onChangeInput(e, employeeId)}
+                // placeholder="Type Position"
+              />
+            </td>
+            <td>
+              <input
+                name="position"
+                type="number"
+                // value={position}
+                // onChange={(e) => onChangeInput(e, employeeId)}
+                // placeholder="Type Position"
+              />
+            </td>
+            <td>
+              <input
+                name="position"
+                type="number"
+                // value={position}
+                // onChange={(e) => onChangeInput(e, employeeId)}
+                // placeholder="Type Position"
+              />
+            </td>
+            <td>
+              <input
+                name="position"
+                type="number"
+                // value={position}
+                // onChange={(e) => onChangeInput(e, employeeId)}
+                // placeholder="Type Position"
+              />
+            </td>
+            <td>
+              <input
+                name="position"
+                type="number"
+                // value={position}
+                // onChange={(e) => onChangeInput(e, employeeId)}
+                // placeholder="Type Position"
               />
             </td>
           </tr>
